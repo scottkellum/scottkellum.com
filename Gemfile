35c9d0055999985aa597c8665b62dc521b84969d
Scott Kellum <scott@scottkellum.com>
2013-03-18 23:19:06 -0400

    Doing some typography work

--- Gemfile
+++ Gemfile
@@ -1,5 +1,11 @@
+source :rubygems
+
 gem "middleman", "~> 3.0.12"
 gem "middleman-blog", "~> 3.2.0"
 
 # For feed.xml.builder
 gem "builder", "~> 3.0.0"
+
+# Sass goodies
+gem "toolkit", "~> 0.2.6"
+gem "color-schemer", "~> 0.2.3"
--- config.rb
+++ config.rb
@@ -31,36 +31,15 @@ page "/feed.xml", :layout => false
 # Compass
 ###
 
-# Susy grids in Compass
-# First: gem install susy
-# require 'susy'
+require 'compass'
+require 'toolkit'
+require 'breakpoint'
+require 'color-schemer'
 
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
+output_style     = :compressed
+line_comments    = false
+preferred_syntax = :scss
 
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-# 
-# With no layout
-# page "/path/to/file.html", :layout => false
-# 
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-# 
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy (fake) files
-# page "/this-page-has-no-template.html", :proxy => "/template-file.html" do
-#   @which_fake_page = "Rendering a fake page with a variable"
-# end
 
 ###
 # Helpers
@@ -85,13 +64,13 @@ set :images_dir, 'images'
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
   
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
   
   # Enable cache buster
-  # activate :cache_buster
+  activate :cache_buster
   
   # Use relative URLs
   # activate :relative_assets
