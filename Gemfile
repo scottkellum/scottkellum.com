6dfe0f2c123475bc382e2801839135be89438b18
Scott Kellum <scott@scottkellum.com>
2013-12-12 12:35:04 -0500

    new article: accessability isnt a buzzword

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ source 'http://rubygems.org'
 
 gem "middleman", "~>3.2.0"
 gem "middleman-blog"
+gem "middleman-minify-html"
 gem "nokogiri"
 
 # Live-reloading plugin
--- config.rb
+++ config.rb
@@ -95,8 +95,14 @@ configure :build do
   # Minify Javascript on build
   activate :minify_javascript
 
+  # Minify html
+  activate :minify_html
+
   # Enable cache buster
   activate :asset_hash
+
+  # gzip
+  activate :gzip
 
   # Use relative URLs
   # activate :relative_assets
