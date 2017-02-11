2433bc184cd6d1d6b3b871ad24df1e0f1d42ad3a
Scott Kellum <scott@scottkellum.com>
2017-02-11 10:53:34 -0500

    canvas tweaks

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -3,11 +3,15 @@ var articleHeight = a.offsetHeight;
 
 var windowHeight = window.innerHeight;
 
-var p   = document.getElementById('progress');
-var svg = document.getElementById('hsvg');
-var c   = document.getElementById('c');
+var p    = document.getElementById('progress');
+var svg  = document.getElementById('hsvg');
+var c    = document.getElementById('c');
+
+var offset = 0;
 
 document.onscroll = function() {
+  var diff = offset - window.pageYOffset;
+  offset = window.pageYOffset;
   if(p) {
     p.style.width = (window.pageYOffset / (document.querySelector('article').offsetHeight - windowHeight) * 100) + '%';
   };
@@ -16,7 +20,7 @@ document.onscroll = function() {
     svg.style.opacity = window.pageYOffset * -.004 + 1;
   };
   if(c) {
-    pixel(10);
+    pixel(10,diff);
   };
 };
 
@@ -34,27 +38,23 @@ if(c) {
   ctx.fillRect(0,0,c.width,c.height);
   ctx.fillStyle = "rgba(0,183,180,.5)";
 
-  pixel(Math.round(c.width * 100));
+  pixel(Math.round(c.width * 100),0);
 }
 
 function seed() {
   return [Math.round(Math.random()*c.width),Math.round(Math.random()*c.height)];
 }
 
-function pixel(f) {
+function pixel(f,o) {
   for (var j = 0; j < f; j++) {
-    draw(0);
-  }
-}
-
-function draw(o) {
-  for (var i=0 in s) {
-    s[i][0] = s[i][0] + Math.round(Math.random()*4) - 2;
-    s[i][1] = s[i][1] + Math.round(Math.random()*4) - 2;
-    if (s[i][0] > c.width)  { s[i][0] = c.width; }
-    if (s[i][0] < 0)       { s[i][0] = 0; }
-    if (s[i][1] > c.height) { s[i][1] = c.height; }
-    if (s[i][1] < 0)       { s[i][1] = 0; }
-    ctx.fillRect(s[i][0],s[i][1],1,1);
+    for (var i=0 in s) {
+      s[i][0] = s[i][0] + Math.round(Math.random()*4) - 2;
+      s[i][1] = s[i][1] + Math.round(Math.random()*4) - 2 + (o/100);
+      if (s[i][0] > c.width)  { s[i][0] = c.width; }
+      if (s[i][0] < 0)       { s[i][0] = 0; }
+      if (s[i][1] > c.height) { s[i][1] = c.height; }
+      if (s[i][1] < 0)       { s[i][1] = 0; }
+      ctx.fillRect(s[i][0],s[i][1],1,1);
+    }
   }
 }
