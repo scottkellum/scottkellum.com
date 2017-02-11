4819006273dd26a3bd767d92e2c8c9d13151ad9a
Scott Kellum <scott@scottkellum.com>
2017-02-10 22:03:04 -0500

    projects stuff

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -4,9 +4,10 @@ var articleHeight = a.offsetHeight;
 var windowHeight = window.innerHeight;
 
 var p = document.getElementById('progress');
-
-console.log(a.t);
+var s = document.getElementById('hsvg');
 
 document.onscroll = function() {
   p.style.width = (window.pageYOffset / (document.querySelector('article').offsetHeight - windowHeight) * 100) + '%';
+  s.style.transform = 'translateY(' + window.pageYOffset * -.5 + 'px)';
+  s.style.opacity = window.pageYOffset * -.004 + 1;
 };
