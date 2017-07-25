525d5c26437c7d31e189c73b4b899bdd93f5da4c
Scott Kellum <scott@scottkellum.com>
2017-07-25 18:25:40 -0400

    Almost there with the content

--- source/javascripts/portfolio.js
+++ source/javascripts/portfolio.js
@@ -1,9 +1,19 @@
 var article = document.getElementById('main');
-var width = window.innerWidth;
-var height = window.innerHeight;
+var viewHeight = window.innerHeight;
 
+var scroller = article.querySelectorAll('.scroller');
 
-// PAGING
-function expand(e) {
-  e.classList.toggle('active');
+article.onscroll = function() {
+  for (var i = 0; i < scroller.length; i++) {
+    var scrollDistance = (scroller[i].offsetParent.offsetTop + scroller[i].offsetTop - article.scrollTop - viewHeight + (scroller[i].offsetHeight/2)) / viewHeight * 100;
+    if(scrollDistance < 0 && scrollDistance > -86.6) {
+      scroller[i].querySelector('img').style.transform = 'translateY(' + scrollDistance + '%)';
+    }
+    else if(scrollDistance > 0) {
+      scroller[i].querySelector('img').style.transform = 'none';
+    }
+    else if(scrollDistance < -86.6) {
+      scroller[i].querySelector('img').style.transform = 'translateY(-86.6%)';
+    }
+  }
 };
