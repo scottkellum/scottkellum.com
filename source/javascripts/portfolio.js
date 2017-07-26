d45e727807ecbdec49727022f9af384549394d04
Scott Kellum <scott@scottkellum.com>
2017-07-25 23:17:00 -0400

    remove scroll events for scrollers

--- source/javascripts/portfolio.js
+++ source/javascripts/portfolio.js
@@ -6,14 +6,10 @@ var scroller = article.querySelectorAll('.scroller');
 article.onscroll = function() {
   for (var i = 0; i < scroller.length; i++) {
     var scrollDistance = (scroller[i].offsetParent.offsetTop + scroller[i].offsetTop - article.scrollTop - viewHeight + (scroller[i].offsetHeight/2)) / viewHeight * 100;
-    if(scrollDistance < 0 && scrollDistance > -86.6) {
-      scroller[i].querySelector('img').style.transform = 'translateY(' + scrollDistance + '%)';
-    }
-    else if(scrollDistance > 0) {
-      scroller[i].querySelector('img').style.transform = 'none';
-    }
-    else if(scrollDistance < -86.6) {
-      scroller[i].querySelector('img').style.transform = 'translateY(-86.6%)';
+    if(scrollDistance < 0 && scrollDistance > -100) {
+      scroller[i].classList.add('animate');
+    } else {
+      scroller[i].classList.remove('animate');
     }
   }
 };
