5fd605a3161650e79642b53ba047a52740df9ba4
Scott Kellum <scott@typetura.com>
2020-12-23 17:03:36 -0800

    tweaks

--- script.js
+++
@@ -1,8 +0,0 @@
-const photos = [
-  ['<a href="https://anamonroe.com/" target="_blank">Ana Monroe</a>','/profile.jpg'],
-  ['<a href="https://tatebot.com/" target="_blank">Tate-Stefan Tozer</a>','/profile2.jpg']
-];
-const photo = photos[Math.floor(Math.random() * photos.length)];
-
-document.getElementById('photoLink').innerHTML = photo[0];
-document.getElementById('photo').setAttribute('src', photo[1]);
