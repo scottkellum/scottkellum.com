e2d61e044d89a1d403ce57bc63e4f9f8947a0a77
Scott Kellum <scott@typetura.com>
2026-04-20 23:04:21 -0700

    WOrk in progres JS behavior

---
+++ src/scripts/keyboard-navigation.js
@@ -0,0 +1,15 @@
+// DO THIS
+// Container.scrollBy({ top: 0, left: 1, behavior: 'smooth' });
+
+// document.addEventListener("keydown", (e) => {
+//   if (e.key !== "ArrowDown" && e.key !== "ArrowUp") return;
+//   e.preventDefault();
+
+//   if (e.key === "ArrowDown") {
+//     const next = positions.find((p) => p > current + threshold);
+//     if (next !== undefined) window.scrollTo({ left: next });
+//   } else {
+//     const prev = [...positions].reverse().find((p) => p < current - threshold);
+//     if (prev !== undefined) window.scrollTo({ left: prev });
+//   }
+// });
